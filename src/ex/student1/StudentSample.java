76496dfc5226c17308ec632401fccc6cab624aab
mhayashi-hub <redbrick.gmad@gmail.com>
2021-10-13 14:44:49 +0900

    2021/10/13 午前課題結果解説と追加３

--- src/ex/student1/StudentSample.java
+++ src/ex/student1/StudentSample.java
@@ -41,7 +41,7 @@ public class StudentSample {
         double mathave = Math.round(mathsum * 100.00) / i /100.00;
         double engave = Math.round(engsum * 100.00) / i / 100.00;
         System.out.println("クラス平均点：国語 "+japave+" 数学 "+mathave+" 英語 "+engave);
-        int j = 2;
+        int j = 4;
         System.out.println(students[j].getName()+":合計点 "+ students[j].sum()+" 平均点 "+ students[j].ave());
     }
 }

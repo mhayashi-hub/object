d09722e755afba18e55b2ea06367422e43bdb8d3
mhayashi-hub <redbrick.gmad@gmail.com>
2021-10-19 15:08:07 +0900

    2021/10/19 午後演習/自習分追加２

--- src/basic/inspection/Result.java
+++ src/basic/inspection/Result.java
@@ -51,14 +51,21 @@ class Result {
     }
 
     // method definition
-    double getMBP() {
+    /*
+    public double getMBP() {
         double mBPBase = dBP + (sBP - dBP) / 3.00;
         double mBP = Math.round(mBPBase * 10.0) / 10.0;
         return mBP;
     };
+    */
+    public double getMBP() {
+      return Math.round((dBP+(sBP-dBP/3.0)) * 10.0) / 10.0;
+    };
+
     // output override
     @Override
     public String toString() {
-        return String.format("%-13s%-8s%19s %2.1f %3d  %4.1f   %4.1f   %4.1f",id,name,dateTime,bT,pulse,sBP,dBP,this.getMBP());
+        return String.format("%-13s%-12s %19s %2.1f %3d  %4.1f   %4.1f   %4.1f",
+                id,name,dateTime,bT,pulse,sBP,dBP,getMBP());
     };
 }
--- src/localtest/fileRead.java
+++ src/localtest/fileRead.java
@@ -1,7 +1,7 @@
 package localtest;
 
 import java.io.BufferedReader;
-import java.io.File;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -12,18 +12,31 @@ class fileRead {
     public static void main(String[] args) {
         Path filePath = Paths.get("C:\\Users\\user.DESKTOP-05BGNA3\\IdeaProjects\\object\\src\\localtest\\test01.txt");
         try (BufferedReader reader = Files.newBufferedReader(filePath, StandardCharsets.UTF_8)) {
-            /*
             for (String line; (line = reader.readLine()) != null; ) {
                 System.out.println(line);
             }
-            */
+            /*
             int i;
             for (i=5 ; i > 0 ;i--) {
                 String line = reader.readLine();
                 System.out.println(line);
             }
+            */
         } catch (IOException ex) {
                 System.err.println(ex);
-            }
+        }
+
+        Path outPath = Paths.get("C:\\Users\\user.DESKTOP-05BGNA3\\IdeaProjects\\object\\src\\localtest\\testout01.txt");
+        try (BufferedWriter writer = Files.newBufferedWriter(outPath, StandardCharsets.UTF_8)) {
+            writer.append("test output to file");
+            writer.newLine();
+            writer.newLine();
+            writer.append("test output to file-2 write to file");
+            writer.newLine();
+            writer.append("これはテスト出力toTextです。");
+
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
     }
 }

8dd1e0b72b79758b42737ade5e8cf50963ebddaf
mhayashi-hub <redbrick.gmad@gmail.com>
2021-11-09 13:18:49 +0900

    2021/11/09 午後独習２前日の続き

--- src/localtest/stringsTest/SplitTestSample.java
+++ src/localtest/stringsTest/SplitTestSample.java
@@ -1,7 +1,88 @@
 package localtest.stringsTest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 class SplitTestSample {
     public static void main(String[] args) {
+        // 区切り文字として指定してその文字で配列に分ける。
+        String testSentence1 = "a,b,c,d";
+        String[] splitResult1 = testSentence1.split(",");
+        for (String result:splitResult1) {
+            System.out.println(result);
+        }
+        System.out.println(splitResult1.length);
+        System.out.println();
+
+        // 区切り文字を入れないで配列に分割すると、一文字ごとに分割した配列になる。
+        String[] splitResult2 = testSentence1.split("");
+        for (String res:splitResult2) {
+            System.out.println(res);
+        }
+        System.out.println(splitResult2.length);
+        System.out.println();
+        // 区切り文字の指定に正規表現パターンも指定できる。
+        String testSentence2 = "a,b:C.d;E,F.G,F";
+        String[] splitResult3 = testSentence2.split("[,:;.]");
+        for (String reply:splitResult3) {
+            System.out.println(reply);
+        }
+        System.out.println(splitResult3.length);
+        System.out.println();
+        // 区切り文字で配列に切り出す際に、切り出す上限数を指定できる。
+        // 上限数を設定すると上限-1までは普通に切り出し、残りを最後の配列に入れる。
+        String[] splitResult4 = testSentence2.split("[,:;.]",4);
+        for (String ret:splitResult4) {
+            System.out.println(ret);
+        }
+        System.out.println(splitResult4.length);
+        System.out.println();
+        // 文字列の末尾が空白の場合も要素として分割する方法。
+        String testSentence3 = "a,b,,c,d,,";
+        String[] splitResult5a = testSentence3.split(",");
+        for (String result:splitResult5a) {
+            System.out.println(result);
+        }
+        System.out.println(splitResult5a.length);
+        System.out.println();
+        // 末尾に区切り文字のみで内容が空白(,,など)の場合に、その空白を要素として取り込んで配列に設定する方法。
+        // 要素制限数に-1を設定する。メソッド側の定義の場合は負数を設定すると配列にパターンに合致するものを
+        // 極力分割して割り当てるようにするらしい。一番有用な事例が文字列末尾の区切り文字連続部分か？
+        // 中間の空白で区切り文字だけ連続した部分は負数の制限値を設定しなくてもきちんと要素として分割されてる。
+        String[] splitResult5b = testSentence3.split(",",-1);
+        for (String result:splitResult5b) {
+            System.out.println(result);
+        }
+        System.out.println(splitResult5b.length);
+        System.out.println();
+
+        // 配列だけでなくリスト化もできる。
+        List<String> list = new ArrayList<>();
+        // 配列に一度分割するのは必須？
+        // String[] alist1 = testSentence3.split(",",-1);
+        // CollectionsクラスのaddAllだとリストに対して配列を要素ごとで追加処理できるっぽい。
+        // リスト 配列の順で指定して配列をリスト化できる。
+        // Collections.addAll(list,alist1);
+        // Collections.addAllを使えば、配列指定する場所で直接splitで配列に分割する処理を書けるので配列宣言不要か？
+        Collections.addAll(list,testSentence3.split(",",-1));
+        // 分割時にasListで受けてArrayList化することができるらしいという説明もあるがこの方法はダメ。
+        // asListで処理したものは配列形式のリストではないものになるため要素削除や追加のできない固定長配列になるため、
+        // リストにする意義が半分くらい失われてしまう。
+        // List<String> list = Arrays.asList(testSentence3.split(",",-1));
+        for (String al:list) {
+            System.out.println(al);
+        }
+        System.out.println(list.size());
+        System.out.println();
+
+        list.add("g");
+        for (String al:list) {
+            System.out.println(al);
+        }
+        System.out.println(list.size());
+        System.out.println();
 
     }
 }
--- src/localtest/stringsTest/ValueOfConvSample.java
+++ src/localtest/stringsTest/ValueOfConvSample.java
@@ -1,4 +1,62 @@
 package localtest.stringsTest;
 
 class ValueOfConvSample {
+    public static void main(String[] args) {
+        int num1 = 100;
+        long num2 = 200;
+        float num3 = 100.25f;
+        double num4 = 120.36;
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        // 数値型を文字列型に変換、String.valueOf()を使用する。
+        // 単純に標準出力に表示するだけでは一見してわからないが、Stringクラスで作ったインスタンスに入れられるので
+        // 文字列型になっていることがわかる。
+        // (消費型インスタンスとしてメソッドの中に入れるとコンパイルエラーやIDEの静的チェックが働きにくくなる？)
+        String string1 = String.valueOf(num1);
+        String string2 = String.valueOf(num2);
+        String string3 = String.valueOf(num3);
+        String string4 = String.valueOf(num4);
+
+        System.out.println(string1);
+        System.out.println(string2);
+        System.out.println(string3);
+        System.out.println(string4);
+        // 文字列から数値への変換、同様に数値型.valueOf()を使う。
+        // またはparse+数値型という個別メソッドも使える。
+        int num11 = Integer.valueOf(string1);
+        int num12 = Integer.parseInt(string1);
+        System.out.println(num11);
+        System.out.println(num12);
+        // 一度文字列変換すると数値の型は情報がなくなるので、longだろうがintに入れられる。
+        // ただしfloat-文字列-intとかだと小数点以下の値が欠落するので注意すること。
+        // またlongの大きな数値をintに入れる場合、桁が合わない部分はやっぱり削られるので注意が必要。
+        long num21 = Long.valueOf(string2);
+        long num22 = Long.parseLong(string2);
+        System.out.println(num21);
+        System.out.println(num22);
+        // 浮動小数点の文字列から数値への変換。注意するのは小数点の桁数削れ等での誤差発生とか。
+        float num31 = Float.valueOf(string3);
+        float num32 = Float.parseFloat(string3);
+        System.out.println(num31);
+        System.out.println(num32);
+
+        double num41 = Double.valueOf(string4);
+        double num42 = Double.parseDouble(string4);
+        System.out.println(num41);
+        System.out.println(num42);
+
+        // 数値型同士の相互変換。
+        int numA = 100;
+        double numB = numA; // 型の違う変数に格納して暗黙的キャストで型変換実施
+        System.out.println(numA);
+        System.out.println(numB); // 同じ整数型で桁がどちらも許容範囲の型変換なので数値の欠落はない。
+        double numC = 100.25;
+        int numD =(int) numC; // 整数型と小数型なので型自体が合わず情報が欠落する。()内に型を指定して明示的キャストで変換。
+        System.out.println(numC);
+        System.out.println(numD);
+
+    }
 }

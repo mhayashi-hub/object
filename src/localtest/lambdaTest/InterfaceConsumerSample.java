e036a9821ed70a345ecdcae0100f6b87e4d7b43a
mhayashi-hub <redbrick.gmad@gmail.com>
2021-11-19 14:30:45 +0900

    2021/11/19 午後lambda振り返り整理４エラーあり

--- src/localtest/lambdaTest/InterfaceConsumerSample.java
+++ src/localtest/lambdaTest/InterfaceConsumerSample.java
@@ -21,5 +21,34 @@ class InterfaceConsumerSample {
                 }
             }
         }.accept(list1);
+        // Overrideあり、匿名クラスでの書き方。forEachメソッドを使って拡張for文の部分はメソッド側に移している。
+        list1.forEach(new Consumer<String> () {
+            @Override
+            public void accept(String t) {
+                System.out.println(t + " です。");
+            }
+        });
+        // 上記の匿名クラスをlambda式にして省略した書き方。
+        list1.forEach((String t) -> {
+            System.out.println(t + " です。");
+        });
+        // 引数の型は入出力で同一のため省略可能。
+        list1.forEach((t) -> {
+            System.out.println(t + " です。");
+        });
+        // 引数が一つだけなので引数をくくる()を省略可能。
+        list1.forEach(t -> {
+            System.out.println(t +" です。");
+        });
+        // {}ブロック内コードが一行で済んでいるので{}を省略して一行にまとめられる。
+        list1.forEach(t -> System.out.println(t + " です。"));
+        // 関数型インターフェースのメソッドと出力部のメソッドのシグネチャ(引数の型、数、並び)が一致していれば、
+        // メソッド参照形式でメソッドに値を渡せる。ただし今回は入力(t)と出力(t + " です。")の内容が違うので無理。
+
+        // lambda式でメソッド参照できる場合の書き方。
+        list1.forEach(System.out::println);
+
+        // lambda式＋streamでマッピングして入力の内容を加工してメソッド参照にできるようにした場合の書き方。
+        list1.stream().map(s -> s + " です。").forEach(System.out::println);
     }
 }
--- src/localtest/lambdaTest/InterfacePredicateSample.java
+++ src/localtest/lambdaTest/InterfacePredicateSample.java
@@ -5,7 +5,7 @@ import java.util.function.Predicate;
 class InterfacePredicateSample {
     public static void main(String[] args) {
         int c1 = 10;
-        /*
+
         boolean bool1 = new Predicate<Integer>() {
             @Override
             public boolean test(Integer c1) {
@@ -21,17 +21,21 @@ class InterfacePredicateSample {
                 // 戻り値はbooleanなのでどちらかを出力することが強制される。
             }
         }.test(c1);
-        */
-        // lambda式記法で書く。
-        Predicate bool1 = (Integer s1) -> {
+        System.out.println(bool1);
+
+        // lambda式記法で書く。-> 書けていない。
+        /*
+        Predicate<Integer> bool1 = (Integer c1) -> {
             boolean b2 = false;
-            if (s1 == 1) {
+            if (c1 == 1) {
                 b2 = true;
             } else {
                 b2 = false;
             };
-            return b2}.test(c1);
+            return b2;}.test(c1);
+         */
+        // boolean bool1 = infPredicate1.test(c1);
 
-        System.out.println(bool1);
+
     }
 }

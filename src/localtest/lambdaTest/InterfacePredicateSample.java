f6f375170093dce7ed90670c3f1ce843a46a6e1f
mhayashi-hub <redbrick.gmad@gmail.com>
2021-11-19 12:15:22 +0900

    2021/11/19 午前lambda振り返り整理３エラーあり

--- src/localtest/lambdaTest/InterfacePredicateSample.java
+++ src/localtest/lambdaTest/InterfacePredicateSample.java
@@ -2,9 +2,10 @@ package localtest.lambdaTest;
 
 import java.util.function.Predicate;
 
-class IntefacePredicateSample {
+class InterfacePredicateSample {
     public static void main(String[] args) {
         int c1 = 10;
+        /*
         boolean bool1 = new Predicate<Integer>() {
             @Override
             public boolean test(Integer c1) {
@@ -20,6 +21,16 @@ class IntefacePredicateSample {
                 // 戻り値はbooleanなのでどちらかを出力することが強制される。
             }
         }.test(c1);
+        */
+        // lambda式記法で書く。
+        Predicate bool1 = (Integer s1) -> {
+            boolean b2 = false;
+            if (s1 == 1) {
+                b2 = true;
+            } else {
+                b2 = false;
+            };
+            return b2}.test(c1);
 
         System.out.println(bool1);
     }

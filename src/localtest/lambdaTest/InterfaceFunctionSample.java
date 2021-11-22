a61c150fa21bc6037f3a43a566f715052196a194
mhayashi-hub <redbrick.gmad@gmail.com>
2021-11-22 12:37:38 +0900

    2021/11/22 午前lambda振り返り整理７エラー残し

--- src/localtest/lambdaTest/InterfaceFunctionSample.java
+++ src/localtest/lambdaTest/InterfaceFunctionSample.java
@@ -1,5 +1,7 @@
 package localtest.lambdaTest;
 
+import basic.sample.IntegerSample;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -13,30 +15,75 @@ class InterfaceFunctionSample {
         list1.add(1000);
         list1.add(100);
         list1.add(50);
+        // 生成したリストがきちんとできているかの出力テスト。
         for (Integer num:list1) {
             System.out.println(num);
         }
-
-        List<Integer> list2 = new Function<List<Integer>,List<Integer>>(){
+        // Functionインターフェースを使用しての実装と出力表示。
+        // Functionインターフェースでの実装の部分、実装によってリストを変換(ここではソート)して新しいリストに出力する。
+        List<Integer> list2 = new Function<List<Integer>,List<Integer>>() {
             @Override
             public List<Integer> apply(List<Integer> list2){
                 Collections.sort(list2);
                 return list2;
             }
         }.apply(list1);
-
+        // 変換したリストの内容を確認する出力テスト。
         for (Integer num:list2) {
             System.out.println(num +" を出力します。");
         }
+        System.out.println();
 
-        // listの内容をソートする部分をFunctionインターフェースでのlamda式に書き直し。
-        /*
-        List<Integer> list2 = (List<Integer> list2) -> {
-            Collections.sort(list2);
-            return list2;
+        // 実装を含めた式を整理。まずlist1に対しての加工メソッドを持つlistを定義する。
+        // applyメソッドを実装したいがどこに実装するかを考えた場合、結果のリストに実装はできない。
+        // (結果のリストはすでにapply相当の処理を適用した後の結果なので、そこに実装しても使えない。)
+        // となると中間処理で加工するインスタンスとして同じList<Integer>型の物を持つべきかと思ったが、
+        // Functionインターフェースの実装なので無名クラス実装としてFunctionを型に取り、
+        // list1convという無名クラスを定義する。この時はapplyでのlist変換が実装できれば良い。
+        Function<List<Integer>, List<Integer>> list1conv = new Function<>() {
+            @Override
+            public List<Integer> apply(List<Integer> list1conv) {
+               Collections.sort(list1conv);
+               return list1conv;
+            }
         };
-         */
+        // リストの加工用(に準備したインスタンスとその中にある)メソッドが実装できたので、それを使ってリストを加工する。
+        List<Integer> list2a = list1conv.apply(list1);
+        // 加工したリストの内容を確認するための出力テスト。
+        for (Integer num:list2a) {
+            System.out.println(num +" を出力します。");
+        }
+        System.out.println();
+
+        // 無名クラス実装部分を省略してラムダ式に変換する。
+        // ラムダ式の中の引数は仮の引数であり実際は外では定義されないし参照できない。あくまでも引数として渡すのに
+        // なにかの枠がいる部 分を埋めるためのものなので、使い捨ての仮の変数でよい。
+        // return部分は一行に変換可能か？<-変換しなくても一応ラムダ式ではある。
+        Function<List<Integer>, List<Integer>> list1conva = (List<Integer> list) -> {
+            Collections.sort(list);
+            return list;
+        };
+        // Functionインターフェースではapplyメソッドしか存在しない(関数型インターフェースではメソッドは一つのみ)ので、
+        // applyの実装部分を書いていてもapplyと書くことを省略できる。
+        // (書いた実装はFunctionであるかぎりapplyに適用されると自明的に判断できるため。
+        //  これが関数型の特徴でラムダ式の省略で活用されている。)
+        List<Integer> list2b = list1conva.apply(list1);
+        for(Integer num:list2b) {
+            System.out.println(num +" を出力します。");
+        };
+        System.out.println();
+        // 結果のリストを生成した後の処理は、変換してみるとforEachで処理するのと同じか？
+        // ここはFunctionではなくConsumerインターフェースでのラムダ式記述が使える。
+        list2b.forEach(n -> System.out.println(n +" を出力します。"));
+        System.out.println();
+
+        // ラムダ式は関数型インターフェースFunctionを利用するための実装を書く部分での無名クラス実装の箇所で
+        // 置き換えが可能、他の処理は無理か？
+        // 一行でリスト生成するには、関数型インターフェースFunctionの処理がリスト生成に直結していないとダメか。
+
         // streamのmapでメソッド参照可能にして、 + lambdaで書きなおした場合。
+        // ソートの場合はsorted()メソッド、出力の加工にはmap()メソッドが使えて、さらに出力部分にforEach、
+        // その中でメソッド参照で標準出力への出力メソッドを呼び出している。
         list1.stream().sorted().map(s -> s +" を出力します。").forEach(System.out::println);
     }
 }
--- src/localtest/lambdaTest/InterfaceSupplierSample.java
+++ src/localtest/lambdaTest/InterfaceSupplierSample.java
@@ -26,10 +26,39 @@ class InterfaceSupplierSample {
         // Override部分を無視して書くと、List<String> list2 = new Supplier<List<String>>.get();となるはず。
         // list2に渡させるのはList<String>を構成する値そのものになるのでList<String>に合致して処理できる。
         // 元からSupplierの中に値が設定されていればそのままでてくるはずだが何もないはずなので実装しないと値が取り出せない。
-
+        // 結果出力部分。拡張for文を使っているのでforEachメソッドに置き換えやすい。
         for (String element:list2) {
             System.out.println(element +" でした。");
         }
+        System.out.println();
+        // 拡張for文の部分はConsumerインターフェースを利用したラムダ式記述にできる。
+        list2.forEach(s -> {System.out.println(s +" でした。"); });
+        System.out.println();
 
-    }
+        // list2を生成するのは関数型インターフェースのgetメソッドになるので、まず関数型インターフェースの部分を作る。
+        Supplier<List<String>> list1conv = new Supplier() {
+            @Override
+            public List<String> get() {
+                List<String> list1 = new ArrayList<>();
+                list1.add("赤");
+                list1.add("緑");
+                list1.add("黄");
+                return list1;
+            }
+        };
+        List<String> list2a = list1conv.get();
+        list2a.forEach(s -> { System.out.println(s + " でした。"); });
+        System.out.println();
+
+        // ラムダ式に変更。
+        Supplier<String> list1conva = ((List<String> list) -> {
+            List<String> clist = new ArrayList<> ();
+            clist.add("赤");
+            clist.add("緑");
+            clist.add("黄");
+            return clist;
+        });
+
+
+        }
 }

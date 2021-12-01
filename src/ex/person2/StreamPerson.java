b54fc68edb2d979e87b77b8431f6dd0b580b41b3
mhayashi-hub <redbrick.gmad@gmail.com>
2021-12-01 12:18:47 +0900

    2021/12/01 午前演習課題１

--- src/ex/person2/StreamPerson.java
+++ src/ex/person2/StreamPerson.java
@@ -2,9 +2,8 @@ package ex.person2;
 
 import basic.sample.enumsample.Gender;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 class StreamPerson {
     public static void main(String[] args) {
@@ -61,5 +60,76 @@ class StreamPerson {
         personStreamList.stream().filter(s -> s.getGender().equals(Gender.WOMEN))
                 .sorted(Comparator.comparing(Person::getName).reversed())
                 .forEach(System.out::println);
+
+        // 課題６
+        System.out.println("課題６");
+        personStreamList.stream().map(s -> s.getName()).forEach(System.out::println);
+        // 課題７ Genderでグループ化したマップを生成する。
+        // 期待するのはGender.MENとGender.WOMENの二通りのマップ生成だが、collect+groupingByだと出力結果は一つのマップ。
+        System.out.println("課題７");
+        Map<Gender,List<Person>> genderGroupListMap = personStreamList.stream()
+                .collect(Collectors.groupingBy(Person::getGender));
+        // System.out.println(genderGroupListMap);
+        // GenderでGroupingした一つのマップを加工して出力して課題の回答と同じ形式で出力。
+        genderGroupListMap.entrySet().stream().map(s -> s.getKey().getJpName() +":"+ s.getValue())
+                .forEach(System.out::println);
+        // GroupingByでグループ化した後で別のマップに分割してみる。
+        Map<Gender,List<Person>> menGroupListMap = personStreamList.stream()
+                .filter(s -> s.getGender().equals(Gender.MEN)).collect(Collectors.groupingBy(Person::getGender));
+        // System.out.println(menGroupListMap);
+        menGroupListMap.entrySet().stream().map(s -> s.getKey().getJpName() +":"+ s.getValue())
+                .forEach(System.out::println);
+        Map<Gender,List<Person>> womenGroupListMap = personStreamList.stream()
+                .filter(s -> s.getGender().equals(Gender.WOMEN)).collect(Collectors.groupingBy(Person::getGender));
+        womenGroupListMap.entrySet().stream().map(s -> s.getKey().getJpName() +":"+ s.getValue())
+                .forEach(System.out::println);
+
+        // 課題８ 最初に考え付いたのはGenderでフィルタして分けてAgeでソートしてから最初に出てくる値をとる方法。
+        System.out.println("課題８");
+        System.out.println(personStreamList.stream().filter(p -> p.getGender().equals(Gender.MEN))
+                        .sorted(Comparator.comparing(Person::getAge).reversed()).findFirst().get());
+        // 課題８別解 Genderでふるい分けした後、単純にmaxを使ってageの最大値をとる方法で書きなおし。
+        // Optional型をget()メソッドで値に変換、nullオブジェクトの場合はnullがそのまま出てしまう危険あり。
+        System.out.println(personStreamList.stream().filter(p -> p.getGender().equals(Gender.MEN))
+                            .max(Comparator.comparing(Person::getAge)).get());
+        // 課題８別解　ifPresentを使ってnullオブジェクトを扱わせないようにする方法。
+        // ifPresent()を使うと、nullでない場合にメソッドの()内の処理を実行して、その処理にOptional型から
+        // 値を取り出して引き渡せるので、get()いらずでnullの例外を出さずに処理ができる。
+        // つまりget()で値に変換するとprintlnに渡す処理をさらに上にかぶせる必要があるが、ifPresent()だと
+        // ()内の処理にprintlnを入れて値として渡せるので、上にprintlnをかぶせる必要がなくなる。
+        personStreamList.stream().filter(p -> p.getGender().equals(Gender.MEN))
+                .max(Comparator.comparing(Person::getAge)).ifPresent(s -> System.out.println(s));
+
+        // 課題９
+        System.out.println("課題９");
+        System.out.println(personStreamList.stream().filter(p -> p.getGender().equals(Gender.WOMEN))
+                            .sorted(Comparator.comparing(Person::getAge)).findFirst().get());
+        // 課題９別解
+        System.out.println(personStreamList.stream().filter(p -> p.getGender().equals(Gender.WOMEN))
+                            .min(Comparator.comparing(Person::getAge)).get());
+        // 課題９別解 ifPresent()を使って書き直し。
+        personStreamList.stream().filter(p -> p.getGender().equals(Gender.WOMEN))
+                .min(Comparator.comparing(Person::getAge)).ifPresent(s -> System.out.println(s));
+
+        // 課題１０
+        System.out.println("課題１０");
+        OptionalDouble womenAgeAverage = personStreamList.stream().filter(p -> p.getGender().equals(Gender.WOMEN))
+                .mapToInt(s -> s.getAge()).average();
+        System.out.println("女性の平均年齢：" + womenAgeAverage.getAsDouble());
+        // 課題１０別解 ifPresentでIntStreamのgetAsDouble()も置き換え可能らしい。多態性の利点か？
+        personStreamList.stream().filter(p -> p.getGender().equals(Gender.WOMEN))
+                .mapToInt(s -> s.getAge()).average()
+                .ifPresent(s -> System.out.println("女性の平均年齢；" + s));
+
+        // 課題１１
+        System.out.println("課題１１");
+        OptionalDouble menAgeAverage = personStreamList.stream().filter(p -> p.getGender().equals(Gender.MEN))
+                .mapToInt(s -> s.getAge()).average();
+        System.out.println("男性の平均年齢：" + menAgeAverage.getAsDouble());
+        // 課題１１別解 ifPresent()内でprintln使って出力。
+        personStreamList.stream().filter(p -> p.getGender().equals(Gender.MEN))
+                .mapToInt(s -> s.getAge()).average()
+                .ifPresent(s -> System.out.println("男性の平均年齢：" + s));
+
     }
 }

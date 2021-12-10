5506fedec720d609b4ce81288eda4e9bc2693492
mhayashi-hub <redbrick.gmad@gmail.com>
2021-12-10 09:51:09 +0900

    2021/12/10 午前演習課題途中

---
+++ src/ex/dateTime/showCalender.java
@@ -0,0 +1,14 @@
+package ex.dateTime;
+
+import java.util.Calendar;
+
+class showCalender {
+    public static void main(String[] args) {
+        // Calendarクラスを使用。
+        // nowと１年、２カ月、３週間、４日前、５時間後、６分前、７秒前を表示。
+        // yyyy年MM時dd日 hh時mm分ss秒 で表示。
+        Calendar nowCalender = Calendar.getInstance();
+        
+        
+    }
+}

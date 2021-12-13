6bfff5351173d7f0f3bd643405f60618d2490d04
mhayashi-hub <redbrick.gmad@gmail.com>
2021-12-13 09:45:16 +0900

    2021/12/12 午前演習課題最後の課題完了

--- src/ex/dateTime/showLeapYears.java
+++ src/ex/dateTime/showLeapYears.java
@@ -5,4 +5,24 @@ class showLeapYears {
     // 条件：西暦を4で割って割り切れる年はうるう年、
     //      ただし、100で割り切れる年はうるう年ではない
     //      ただし、400で割り切れる年はうるう年となる
+    public static void main(String[] args) {
+        int checkYear = 1999;
+        for (int i = 0;i<=100;i++) {
+            checkYear++;
+            System.out.println("チェック対象年："+ checkYear);
+            if (checkYear%4 == 0) {
+                if (checkYear%100 == 0) {
+                    if (checkYear%400 == 0) {
+                        System.out.println("うるう年です。判定対象：" + checkYear);
+                    } else {
+                        System.out.println("うるう年ではありません。判定対象：" + checkYear);
+                    }
+                } else {
+                    System.out.println("うるう年です。判定対象："+ checkYear);
+                }
+            } else {
+                System.out.println("うるう年ではありません。判定対象："+ checkYear);
+            }
+        }
+    }
 }
